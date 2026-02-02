dffd4725952399c176e46dcfd9b29a844877adef
Sankalp Hegde <103136923+barch0206@users.noreply.github.com>
2026-02-02 15:12:12 +0000

    feat: project initialisation

---
+++ joy-as-a-service/src/main/java/jar/JoyAsAServiceApplication.java
@@ -0,0 +1,13 @@
+package jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyAsAServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoyAsAServiceApplication.class, args);
+	}
+
+}
---
+++ joy-as-a-service/src/test/java/jar/JoyAsAServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyAsAServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
